dc9a7cba869bcf25e17e682d4dfe51988326f750
Masis Osipyan <57831374+MasisLinux@users.noreply.github.com>
2020-03-19 21:08:38 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    let x = document.getElementById("myInput");
+    let y = document.getElementById("hide1");
+    let z = document.getElementById("hide2");
+
+    if(x.type === 'password') {
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else {
+        x.type = "password";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+}
